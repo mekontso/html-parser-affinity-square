ef64f00bd1dccacd43c8b49ff022f0fb283cbc89
mekontso <mekontsoanderson@gmail.com>
2024-06-19 09:05:29 +0100

    Initial commit

---
+++ src/main/java/com/mas/app/HtmlParserAffinitySquareApplication.java
@@ -0,0 +1,13 @@
+package com.mas.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtmlParserAffinitySquareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtmlParserAffinitySquareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mas/app/HtmlParserAffinitySquareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mas.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtmlParserAffinitySquareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
